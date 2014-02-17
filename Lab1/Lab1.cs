a1049727000159dbc5bc78e52281f258203068cc
Станислав <stanislavvs@bk.ru>
2014-02-17 09:37:02 -0800

    Create Lab1.cs

---
+++ Lab1/Lab1.cs
@@ -0,0 +1,16 @@
+
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // задержка экрана
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
